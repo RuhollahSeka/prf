778105868967d6b852de6fbfe33cce0f95daf8fe
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2022-06-06 20:04:31 +0430

    Add TimedModel abstract class

---
+++ common/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class TimedModel(models.Model):
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    updated = models.DateTimeField(
+        auto_now=True,
+    )
+
+    class Meta:
+        abstract = True
