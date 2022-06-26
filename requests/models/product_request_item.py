b0fb28ea7ef40d06338103bfde546d539540eb9d
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2022-06-27 03:43:41 +0430

    Add product request field to product request item model

--- requests/models/product_request_item.py
+++ requests/models/product_request_item.py
@@ -2,9 +2,16 @@ from django.db import models
 
 from common.models import TimedModel
 from products.models import Product
+from requests.models import ProductRequest
 
 
 class ProductRequestItem(TimedModel):
+    product_request = models.ForeignKey(
+        to=ProductRequest,
+        on_delete=models.CASCADE,
+        related_name='items',
+    )
+
     product = models.ForeignKey(
         to=Product,
         on_delete=models.CASCADE,
