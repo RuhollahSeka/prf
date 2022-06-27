656557c0cf60c2e00829aa71b7b34cfcd5807a4d
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2022-06-27 18:11:03 +0430

    Add redis to project

--- prf/settings.py
+++ prf/settings.py
@@ -115,3 +115,13 @@ REST_FRAMEWORK = {
     'SEARCH_PARAM': 'q',
     'PAGE_SIZE': 20,
 }
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+    }
+}
