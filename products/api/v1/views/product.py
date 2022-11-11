c3d14ba2966d2c1e8054199a8c18ea01f7f0205b
Ruhollah Sekaleshfar <r.sekaleshfar@fastbundle.co>
2022-11-11 19:27:10 +0330

    Add document api

--- products/api/v1/views/product.py
+++ products/api/v1/views/product.py
@@ -1,18 +1,43 @@
-from rest_framework.filters import SearchFilter
-from rest_framework.generics import ListAPIView
-from rest_framework.pagination import PageNumberPagination
-from rest_framework_simplejwt.authentication import JWTAuthentication
+# from rest_framework.filters import SearchFilter
+# from rest_framework.generics import ListAPIView
+# from rest_framework.pagination import PageNumberPagination
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+#
+# from products.models import Variant
+# from users.permissions import IsRequester
+# from ..serializers import VariantSerializer
+#
+#
+# class VariantListAPIView(ListAPIView):
+#     permission_classes = (IsRequester,)
+#     authentication_classes = (JWTAuthentication,)
+#     serializer_class = VariantSerializer
+#     filter_backends = (SearchFilter,)
+#     search_fields = ('code',)
+#     queryset = Variant.objects.all()
+#     pagination_class = PageNumberPagination
+from elasticsearch_dsl import Q
+from elasticsearch_dsl.query import Bool, Match
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from products.models import Variant
-from users.permissions import IsRequester
-from ..serializers import VariantSerializer
+from products.documents import VariantDocument
 
 
-class VariantListAPIView(ListAPIView):
-    # permission_classes = (IsRequester,)
-    # authentication_classes = (JWTAuthentication,)
-    serializer_class = VariantSerializer
-    filter_backends = (SearchFilter,)
-    search_fields = ('code',)
-    queryset = Variant.objects.all()
-    pagination_class = PageNumberPagination
+class VariantListAPIView(APIView):
+    http_method_names = ('get',)
+
+    def get(self, request, *args, **kwargs):
+        query = request.query_params.get('q', '')
+        es_query = f'*{query}*'
+        results = VariantDocument.search().query(Q(
+            'bool',
+            should=[Q('wildcard', name=es_query), Q('wildcard', code=es_query)]
+        ))
+        data = []
+        for result in results:
+            data.append({
+                'name': result.name,
+                'code': result.code,
+            })
+        return Response(data=data)
---
+++ products/documents/__init__.py
@@ -0,0 +1,1 @@
+from .variant import VariantDocument
